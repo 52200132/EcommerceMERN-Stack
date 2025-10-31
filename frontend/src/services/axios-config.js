2cae425669ab0aeed2151867d32ff17de91c4aca
PhuocPhung <phuocphung5890@gmail.com>
2025-10-31 20:34:40 +0700

    docker 8h34 pm

--- frontend/src/services/axios-config.js
+++ frontend/src/services/axios-config.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import Wobbling from 'Admin/components/layout/wobbling';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'http://localhost:3000/api/',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 })
